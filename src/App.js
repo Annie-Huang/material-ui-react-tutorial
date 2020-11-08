c62e2ac3e62d353f66ddb48e5770e1343edc4ac3
Annie Huang <annie.hn@gmail.com>
2020-11-08 17:54:42 +1100

    Add ButtonGroup component from material UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from '@material-ui/core/Button';
 import React from "react";
 import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 function App() {
   return (
@@ -15,6 +17,17 @@ function App() {
         <Button startIcon={<SaveIcon />} endIcon={<SaveIcon />} size='large' variant="contained" color="secondary">
           Hello World
         </Button>
+        <br/>
+        <br/>
+        <ButtonGroup>
+          <Button startIcon={<SaveIcon />} size='large' variant="contained" color="primary">Save</Button>
+          <Button startIcon={<DeleteIcon />} size='large' variant="contained" color="secondary">Discard</Button>
+        </ButtonGroup>
+        <br/>
+        <ButtonGroup variant="contained" color="primary">
+          <Button startIcon={<SaveIcon />} size='large'>Save</Button>
+          <Button startIcon={<DeleteIcon />} size='large'>Discard</Button>
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
