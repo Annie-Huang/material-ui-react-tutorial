d6225e1df19c61a017b662b177df6ff754ebf44d
Annie Huang <annie.hn@gmail.com>
2020-11-09 21:35:48 +1100

    Wrapped <Checkbox> into a <FormControlLabel>. and show different icon you can choose for checked and unchecked state icon.

--- src/App.js
+++ src/App.js
@@ -6,18 +6,44 @@ import React from "react";
 import SaveIcon from '@material-ui/icons/Save'
 import DeleteIcon from '@material-ui/icons/Delete'
 import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 function CheckboxExample() {
   const [checked, setChecked] = React.useState(true);
   return (
-    <div>
-      {/*<Checkbox checked={checked} onChange={e => setChecked(e.target.checked)} color="primary" disabled/>*/}
-      <Checkbox
-        checked={checked}
-        onChange={e => setChecked(e.target.checked)}
-        inputProps={{ 'aria-label': 'secondary checkbox' }}
-      />
-    </div>
+    // <div>
+    //   {/*<Checkbox checked={checked} onChange={e => setChecked(e.target.checked)} color="primary" disabled/>*/}
+    //   <Checkbox
+    //     checked={checked}
+    //     onChange={e => setChecked(e.target.checked)}
+    //     inputProps={{ 'aria-label': 'secondary checkbox' }}
+    //   />
+    // </div>
+
+    <>
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          onChange={e => setChecked(e.target.checked)}
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
+        />
+      }
+      label="Testing Checkbox"
+    />
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          icon={<DeleteIcon />}
+          checkedIcon={<SaveIcon />}
+          onChange={e => setChecked(e.target.checked)}
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
+        />
+      }
+      label="Testing Checkbox"
+    />
+    </>
   );
 }
 
