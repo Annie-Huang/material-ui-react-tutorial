65672c56d76193c0194e4fb26a39600985f1f712
Annie Huang <annie.hn@gmail.com>
2020-11-10 17:36:12 +1100

    Use "makeStyles" to create your own style and use it to override some styles from material ui component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from '@material-ui/core/Button';
@@ -8,6 +7,23 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B, #FF8E53)',
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color: 'white',
+    padding: '5px 30px',
+  },
+});
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>
+}
 
 function CheckboxExample() {
   const [checked, setChecked] = React.useState(true);
@@ -52,6 +68,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ButtonStyled />
+        <br/>
+
         <TextField variant="filled" />
         <TextField variant="outlined" color="secondary" type="date" />
         <TextField variant="outlined" color="secondary" type="time" />
